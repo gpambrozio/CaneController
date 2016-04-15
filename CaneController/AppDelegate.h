e495c994886227f33cccbd9f5f5d943387d89202
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2016-04-14 23:27:03 -0700

    Initial Commit

---
+++ CaneController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CaneController
+//
+//  Created by Gustavo Ambrozio on 4/14/16.
+//  Copyright © 2016 Gustavo Ambrozio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
